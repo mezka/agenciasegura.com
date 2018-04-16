4f129906c4b577fe38279a87bccf9d458f78bda8
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2018-04-16 13:19:10 -0300

    fixing even more syntax errors, changed from 'new mysqli_connect' to 'new mysqli'

--- base_datos.php
+++ base_datos.php
@@ -1,7 +1,7 @@
 <?php
 
 /* connecting to mysql database */
-$conn=new mysqli_connect("localhost", "mezka", "fujitsu", "agenciasegura");
+$conn=new mysqli("localhost", "mezka", "fujitsu", "agenciasegura");
 
 if ($conn->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
